81c0f2a054aeea5ddb23ab49b9417563b9a94370
alonar-osu <rudchena@oregonstate.edu>
2018-11-21 09:16:43 -0500

    Refactor: add compareImages function

--- imagecompar.cpp
+++ imagecompar.cpp
@@ -17,8 +17,9 @@
 using namespace cv;
 using namespace std;
 
-
 int validInput(int argc, Mat image1, Mat image2);
+Mat compareImages(Mat image1, Mat image2);
+
 
 int main(int argc, char** argv)
 {
@@ -27,35 +28,15 @@ int main(int argc, char** argv)
 	Mat image1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 	Mat image2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
 
-	// check number of arguments, images
+	// check input data
 	if (!validInput(argc, image1, image2))
 	{
 		return -1;
 	}		
 
-	// create a blank difference image 
-	Mat diffimage(image1.rows, image1.cols, CV_8UC3, Scalar(0, 0, 0));
+	// get difference between 2 images
+	Mat diffimage = compareImages(image1, image2);
 
-	// loop through the pixels of images
-	for (int i = 0; i < image1.rows; i++)
-	{
-		for (int j = 0; j < image1.cols; j++)
-		{
-			// get current pixel's intensity in image 1 						
-			Vec3b intensity1 = image1.at<Vec3b>(Point(j, i));
-
-			// get current pixel's intensity in image 2
-			Vec3b intensity2 = image2.at<Vec3b>(Point(j, i));
-
-			// set difference image's pixel to the difference of image1 and image2
-			// subtract from 255 to have white background	
-			diffimage.at<Vec3b>(Point(j, i))[0] = 255 - abs(intensity1.val[0] - intensity2.val[0]);
-			diffimage.at<Vec3b>(Point(j, i))[1] = 255 - abs(intensity1.val[1] - intensity2.val[1]);
-			diffimage.at<Vec3b>(Point(j, i))[2] = 255 - abs(intensity1.val[2] - intensity2.val[2]); 
-			
-			}
-
-		}
 
 	// set difference image grayscale
 	Mat diffgrey;
@@ -98,3 +79,31 @@ int validInput(int argc, Mat image1, Mat image2)
 return 1;
 }
 
+Mat compareImages(Mat image1, Mat image2)
+{
+
+	// create a blank difference image 
+	Mat diffimage(image1.rows, image1.cols, CV_8UC3, Scalar(0, 0, 0));
+
+	// loop through the pixels of images
+	for (int i = 0; i < image1.rows; i++)
+	{
+		for (int j = 0; j < image1.cols; j++)
+		{
+			// get current pixel's intensity in image 1 						
+			Vec3b intensity1 = image1.at<Vec3b>(Point(j, i));
+
+			// get current pixel's intensity in image 2
+			Vec3b intensity2 = image2.at<Vec3b>(Point(j, i));
+
+			// set difference image's pixel to the difference of image1 and image2
+			// subtract from 255 to have white background	
+			diffimage.at<Vec3b>(Point(j, i))[0] = 255 - abs(intensity1.val[0] - intensity2.val[0]);
+			diffimage.at<Vec3b>(Point(j, i))[1] = 255 - abs(intensity1.val[1] - intensity2.val[1]);
+			diffimage.at<Vec3b>(Point(j, i))[2] = 255 - abs(intensity1.val[2] - intensity2.val[2]); 
+		}
+	}
+
+return diffimage;
+
+}
